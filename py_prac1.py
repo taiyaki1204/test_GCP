70bc9f0ee6e33080e375221d768b4ce79623d923
taiyaki1204 <taiyaki_ueno@instance-kgl.us-central1-c.c.famous-infinity-227205.internal>
2019-01-07 20:46:14 +0900

    add python practice day1

---
+++ py_prac1.py
@@ -0,0 +1,42 @@
+#python practice a days
+#https://www.kaggle.com/colinmorris/learn-python-challenge-day-1/notebook?utm_medium=email&utm_source=mailchimp&utm_campaign=ongoing+python+course
+
+#if sentence
+if 4 > 0:
+ print("if sentence!")
+
+# char *
+print("spam " * 4)
+
+# num type confirm
+type(19.05)
+
+# Quotient of a and b
+print(7 / 2)
+
+# Quotient of a and b, removing fractional parts
+print(7 // 2)
+
+# a raised to the power of b
+print(2**9)
+
+print(min(1,2,3))
+print(max(1,2,3))
+
+# zettaiti
+print(abs(32))
+print(abs(-32))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
